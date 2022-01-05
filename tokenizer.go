0cb3ca86d02d5d5340bbd53e20455be38c77c49c
davhojt <davhojt@gmail.com>
2022-01-06 00:21:51 +0200

    feat: merges tokens

--- tokenizer.go
+++ tokenizer.go
@@ -94,3 +94,133 @@ func tokenize(str string) []token {
 	}
 	return tokens
 }
+
+// TODO: TEST
+func correctArticle(article, noun string) string {
+	type sound int64
+
+	const (
+		Consonant sound = iota
+		Vowel
+		NonLetter
+	)
+
+	soundType := func(r rune) sound {
+		vowels := map[rune]bool{
+			'a': false,
+			'e': false,
+			'i': false,
+			'o': false,
+			'u': false,
+			'h': false,
+		}
+
+		if unicode.IsLetter(r) {
+			_, isVowel := vowels[unicode.ToLower(r)]
+			if isVowel {
+				return Vowel
+			}
+			return Consonant
+		}
+
+		return NonLetter
+	}
+
+	wordBeginsWith := func(str string) sound {
+		var r rune
+		if len(str) > 0 {
+			runes := []rune(str)
+			r = runes[0]
+		}
+		return soundType(r)
+	}
+
+	if len(noun) > 0 {
+		if toLowerCase(article) == "a" && wordBeginsWith(noun) == Vowel {
+			return article[0:1] + "n"
+		}
+
+		if toLowerCase(article) == "an" && wordBeginsWith(noun) == Consonant {
+			return article[0:1]
+		}
+	}
+
+	return article
+}
+
+// TODO: TEST
+func mergeTokens(tokens []token) string {
+	str := ""
+
+	inQuoute := false
+
+	for i := range tokens {
+		buffer := ""
+		spaceBefore := ""
+		spaceAfter := ""
+
+		writeString := func() {
+			str += spaceBefore + buffer + spaceAfter
+			buffer, spaceBefore, spaceAfter = "", "", ""
+		}
+
+		// Adds punctuation
+		if tokens[i].kind == Punctuation {
+			buffer = tokens[i].str
+			spaceAfter = " "
+		}
+
+		if tokens[i].kind == Other {
+			// TODO: This runs every time, even if the current word is not an article.
+			// Indefinite article check
+			for nextToken := i + 1; nextToken < len(tokens); nextToken++ {
+				if tokens[nextToken].kind == Punctuation {
+					break
+				}
+
+				if tokens[nextToken].kind == Other {
+					tokens[i].str = correctArticle(tokens[i].str, tokens[nextToken].str)
+					break
+				}
+			}
+
+			buffer = tokens[i].str
+
+			// Add space after word
+			for nextToken := i + 1; nextToken < len(tokens); nextToken++ {
+				if tokens[nextToken].kind == WhiteSpace || tokens[nextToken].kind == Operation {
+					continue
+				} else if tokens[nextToken].kind == Other {
+					spaceAfter = " "
+				} else {
+					break
+				}
+			}
+		}
+
+		// Adds quotes
+		if tokens[i].kind == Quote {
+			buffer = tokens[i].str
+			if i == 0 || tokens[i-1].kind == WhiteSpace || i >= len(tokens)-1 || tokens[i+1].kind == WhiteSpace {
+				if inQuoute {
+					spaceAfter = " "
+				} else {
+					spaceBefore = " "
+				}
+				inQuoute = !inQuoute
+			}
+		}
+
+		// Removes spaces from start and end of string
+		if i == 0 {
+			spaceBefore = ""
+		}
+		if i == len(tokens)-1 {
+			spaceAfter = ""
+		}
+
+		writeString()
+	}
+
+	return str
+}
